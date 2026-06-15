3334b710e671173d61fefbf0572ec80069e03cd4
Aatishprabhu <prabhuaatish09@gmail.com>
2026-06-15 14:23:06 +0530

    second commit

--- student_project/urls.py
+++ student_project/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('students.urls')),
+    path('', include('student_project.students.urls')),
 ]
